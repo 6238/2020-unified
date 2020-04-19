3e9312ae8f50a0f1f70a6f9bd58b8247e755032f
Ishan Madan <ishan@mistlogic.com>
2020-04-19 16:31:01 -0700

    ishan's code from 4-19-20 meeting (mocking, successfully creating & running tests relating to motor control)

--- learn/ishan/UnitTestCommand/src/test/java/frc/robot/MainTest.java
+++ learn/ishan/UnitTestCommand/src/test/java/frc/robot/MainTest.java
@@ -1,42 +1,59 @@
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import org.mockito.*;
+import static org.mockito.Mockito.*;
+
 import org.junit.*;
+
 import frc.robot.subsystems.*;
 
 public class MainTest {
 
+    TalonFactory f;
+    
+    WPI_TalonSRX front;
+    WPI_TalonSRX back;
+
+    @Before
+    public void setup() {
+        f = mock(TalonFactory.class);
+        front = mock(WPI_TalonSRX.class);
+        back = mock(WPI_TalonSRX.class);
+        
+        when(f.create(0)).thenReturn(front);
+        when(f.create(1)).thenReturn(back);
+    }
+
     @Test
-    public void testElevatorMotorRunsWhenElevatorStarted() {
-        MotorControllerFactory f = new MotorControllerFactory();
-
-        Assert.assertEquals(f.getMotorControllers().size(), 0);
-
-        ExampleSubsystem e = new ExampleSubsystem(f);
-
-        Assert.assertEquals(f.getMotorControllers().size(), 2);
-
-        e.start();
-
-        e.periodic();
-
-        Assert.assertEquals(f.getMotorControllers().size(), 2);
-        Assert.assertEquals(f.getMotorControllers().get(0).getSpeed(), 0.5, 0.0001);
-        Assert.assertEquals(f.getMotorControllers().get(0).getCanBusID(), 0);
-        Assert.assertEquals(f.getMotorControllers().get(1).getSpeed(), -0.5, 0.0001);
-        Assert.assertEquals(f.getMotorControllers().get(1).getCanBusID(), 1);
+    public void testElevatorCreatesMotorControllers() {
+        ElevatorSubsystem e = new ElevatorSubsystem(f);
+        
+        verify(f).create(0);
+        verify(f).create(1);
     }
     
-    public void testElevatorMotorStopsWhenSubsystemStopped() {
-        MotorControllerFactory f = new MotorControllerFactory();
-        ExampleSubsystem e = new ExampleSubsystem(f);
-
-        e.start();
-        e.periodic();
+    @Test
+    public void testElevatorStopCausesMotorsToStop() {
+        ElevatorSubsystem e = new ElevatorSubsystem(f);
+        
         e.stop();
         e.periodic();
 
-        Assert.assertEquals(f.getMotorControllers().size(), 2);
-        Assert.assertEquals(f.getMotorControllers().get(0).getSpeed(), 0, 0.0001);
-        Assert.assertEquals(f.getMotorControllers().get(1).getSpeed(), 0, 0.0001);
+        verify(front).set(ControlMode.PercentOutput, 0.0);
+        verify(back).set(ControlMode.PercentOutput, -0.0);
+    }
+    
+    @Test
+    public void testElevatorStartCausesMotorsToStart() {
+        ElevatorSubsystem e = new ElevatorSubsystem(f);
+        
+        e.start();
+        e.periodic();
+
+        verify(front).set(ControlMode.PercentOutput, 0.5);
+        verify(back).set(ControlMode.PercentOutput, -0.5);
     }
 }
