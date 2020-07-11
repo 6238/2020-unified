6927862152a87f517b9b7d0e94f5c4f78c62896f
Ishan Madan <ishan@mistlogic.com>
2020-07-11 13:19:27 -0700

    added pid control (disabled by default) and a graph for rpm

--- learn/ishan/ElevatorAndShooter/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ learn/ishan/ElevatorAndShooter/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -7,24 +7,90 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMax;
+import java.util.Map;
 
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANPIDController;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.ControlType;
+
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.ToggleButton;
 
 public class ShooterSubsystem extends SubsystemBase {
-  CANSparkMax m_shooterLeft;
-  CANSparkMax m_shooterRight;
+	CANSparkMax m_shooterLeft;
+	CANSparkMax m_shooterRight;
+	CANPIDController m_pidController;
+	CANEncoder m_encoder;
+	double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput, maxRPM, rpm;
 
-  public ShooterSubsystem(Factory f) {
-    m_shooterLeft = f.createSpark(Constants.kLeftShooterSpark);
-    m_shooterRight = f.createSpark(Constants.kRightShooterSpark);
-    m_shooterRight.follow(m_shooterLeft, true);
-    System.out.println("shooter");
-  }
+	NetworkTableEntry kPEntry, kIEntry, kDEntry, kIzEntry, kFFEntry, kMaxOutputEntry, kMinOutputEntry, maxRPMEntry, rpmEntry;
+	
+	boolean pidToggle = false;
+	ToggleButton pidToggleButton = new ToggleButton("pidToggle", pidToggle);
 
-  public void shooter(double speed) {
-    m_shooterLeft.set(speed);
-    System.out.println("shooting");
-  }
+	public ShooterSubsystem(Factory f) {
+		m_shooterLeft = f.createSpark(Constants.kLeftShooterSpark);
+		m_shooterRight = f.createSpark(Constants.kRightShooterSpark);
+		m_shooterRight.follow(m_shooterLeft, true);
+
+		m_pidController = m_shooterLeft.getPIDController();
+		m_encoder = m_shooterLeft.getEncoder();
+
+		kMaxOutput = 1;
+		kMinOutput = -1;
+		maxRPM = 5700;
+
+		kPEntry = Constants.kTab.add("kP", kP).getEntry();
+		kIEntry = Constants.kTab.add("kI", kI).getEntry();
+		kDEntry = Constants.kTab.add("kD", kD).getEntry();
+		kIzEntry = Constants.kTab.add("kIz", kIz).getEntry();
+		kFFEntry = Constants.kTab.add("kFF", kFF).getEntry();
+		kMaxOutputEntry = Constants.kTab.add("kMaxOutput", kMaxOutput).withProperties(Map.of("min", -1, "max", 1)).getEntry();
+		kMinOutputEntry = Constants.kTab.add("kMinOutput", kMinOutput).withProperties(Map.of("min", -1, "max", 1)).getEntry();
+		maxRPMEntry = Constants.kTab.add("maxRPM", maxRPM).withProperties(Map.of("min", 0, "max", 5700)).getEntry();
+		rpmEntry = Constants.kTab.add("rpm", rpm).withWidget(BuiltInWidgets.kGraph).getEntry();
+		periodic();
+	}
+
+	@Override
+	public void periodic() {
+		pidToggle = pidToggleButton.get();
+
+		// read PID coefficients from SmartDashboard
+		double p = kPEntry.getDouble(kP);
+		double i = kIEntry.getDouble(kI);
+		double d = kDEntry.getDouble(kD);
+		double iz = kIzEntry.getDouble(kIz);
+		double ff = kFFEntry.getDouble(kFF);
+		double max = kMaxOutputEntry.getDouble(kMaxOutput);
+		double min = kMinOutputEntry.getDouble(kMinOutput);
+	
+		// if PID coefficients on SmartDashboard have changed, write new values to controller
+		if((p != kP)) { m_pidController.setP(p); kP = p; }
+		if((i != kI)) { m_pidController.setI(i); kI = i; }
+		if((d != kD)) { m_pidController.setD(d); kD = d; }
+		if((iz != kIz)) { m_pidController.setIZone(iz); kIz = iz; }
+		if((ff != kFF)) { m_pidController.setFF(ff); kFF = ff; }
+		if((max != kMaxOutput) || (min != kMinOutput)) { 
+		  m_pidController.setOutputRange(min, max); 
+		  kMinOutput = min; kMaxOutput = max; 
+		}
+
+		maxRPM = maxRPMEntry.getDouble(maxRPM);
+
+		rpm = m_encoder.getVelocity();
+		rpmEntry.setDouble(rpm);
+	}
+
+	public void shooter(double speed) {
+		if (!pidToggle) {
+			m_shooterLeft.set(speed);
+		} else {
+			m_pidController.setReference(maxRPM, ControlType.kVelocity);
+		}
+	}
 }
