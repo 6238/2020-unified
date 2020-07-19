0c81f444238538e8cd968e6aecb58623782b4d2e
VishnuVelayuthan <vishnuvelayuthan@gmail.com>
2020-07-18 23:45:48 -0700

    Now we are back

---
+++ learn/VishDaGod/2020CommandBasedRobot/src/main/java/frc/robot/subsystems/WPI_TalonFactory.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import java.util.*;
+
+//This Class is a Talon Factory in case of mocking
+//Not sure if I will do this today or something
+public class WPI_TalonFactory {
+
+    private ArrayList<WPI_TalonSRX> talons = new ArrayList<WPI_TalonSRX>();
+    private ArrayList<Integer> canBusIDs = new ArrayList<Integer>(); // Will implement later, after talking to Ishan 
+
+    public final WPI_TalonSRX create(int canBusID) {
+        talons.add(new WPI_TalonSRX(canBusID));
+        return talons.get(talons.size() - 1);
+    }
+}
