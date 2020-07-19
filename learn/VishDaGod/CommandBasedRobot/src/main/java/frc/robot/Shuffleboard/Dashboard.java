555ad2c39d3966655a869710672ba3c8988a7043
VishnuVelayuthan <vishnuvelayuthan@gmail.com>
2020-07-18 23:33:51 -0700

    returning to a normal state

--- learn/VishDaGod/CommandBasedRobot/src/main/java/frc/robot/Shuffleboard/Dashboard.java
+++
@@ -1,6 +0,0 @@
-public class Dashboard {
-    
-    //Drive Subsystem Entries
-    public Slider insanityFactorEntry = new Slider("Insanity Factor", DriveConstants.kDefaultValue_InsanityFactor);
-    public Slider sensitivityFactorEntry = new Slider("Sensitivty Factor", DriveConstants.kDefaultValue_SensitivityFactor);
-}
