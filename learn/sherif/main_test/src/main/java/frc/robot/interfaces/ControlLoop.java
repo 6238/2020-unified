b2b426abd0d10432067a2549359e679dbbdf516d
Sherif Abdou <sherif_abdou@outlook.com>
2020-08-02 11:46:33 -0700

    Removed Unnecessary code

--- learn/sherif/main_test/src/main/java/frc/robot/interfaces/ControlLoop.java
+++
@@ -1,8 +0,0 @@
-package frc.robot.interfaces;
-
-public interface ControlLoop {
-    double getDelta(double current_time, double desired_position, double current_position);
-    double getLastTime();
-    void setLastTime(double last_time);
-    void reset();
-}
